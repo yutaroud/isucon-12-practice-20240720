4dd1ad44fadb83de0339d53f41ac7870ba4f53e9
YukiWatanabe <yutaroud@gmail.com>
2024-07-20 17:52:54 +0900

    test

--- go/isuports.go
+++ go/isuports.go
@@ -1345,7 +1345,8 @@ func competitionRankingHandler(c echo.Context) error {
 		"INSERT INTO visit_history_alternative (player_id, tenant_id, competition_id, created_at) VALUES (?, ?, ?, ?)",
 		v.playerID, tenant.ID, competitionID, now,
 	); err != nil {
-		return fmt.Errorf("error Insert visit_history", err,)
+		aaa
+		return fmt.Errorf("error Insert visit_history", err)
 	}
 
 	var rankAfter int64
